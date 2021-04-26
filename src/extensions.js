6d2acd9c93a3037ad4d5a6bf34223df994e26718
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2021-04-26 18:22:40 +0530

    Add sourceUrl

--- src/extensions.js
+++ src/extensions.js
@@ -1,6 +1,7 @@
 extensions = [
     {
         sourceId: 1,
+        sourceUrl: "https://boxnovel.com/",
         sourceName: "BoxNovel",
         sourceCover:
             "https://boxnovel.com/wp-content/uploads/2018/04/BoxNovel-1.png",
@@ -9,6 +10,7 @@ extensions = [
     },
     {
         sourceId: 2,
+        sourceUrl: "https://www.readlightnovel.org/",
         sourceName: "ReadLightNovel",
         sourceCover: "https://www.readlightnovel.org/assets/images/logo6.png",
         sourceLanguage: "English",
@@ -16,6 +18,7 @@ extensions = [
     },
     {
         sourceId: 3,
+        sourceUrl: "https://fastnovel.net/",
         sourceName: "FastNovel",
         sourceCover:
             "https://github.com/rajarsheechatterjee/lnreader-extensions/blob/main/src/en/fastnovel/icon.png?raw=true",
@@ -24,6 +27,7 @@ extensions = [
     },
     {
         sourceId: 4,
+        sourceUrl: "https://readnovelfull.com/",
         sourceName: "ReadNovelFull",
         sourceCover:
             "https://github.com/rajarsheechatterjee/lnreader-extensions/blob/main/src/en/readnovelfull/icon.png?raw=true",
