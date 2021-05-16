4638229a9c549a72e44daf7f2da8d06ebe67d866
Bread Genie <msuhailbh07@gmail.com>
2021-05-16 17:59:41 +0530

    Add user agent
    Add user agent to try circumventing DDoS protection.

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,12 @@
 const chromium = require("chrome-aws-lambda");
 const puppeteer = require("puppeteer");
+const UserAgent = require("user-agents");
+
+const userAgent = new UserAgent();
 
 const scraper = async (url) => {
     const browser = await puppeteer.launch({
-        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security", "--user-agent=" + userAgent + ""],
         defaultViewport: chromium.defaultViewport,
         executablePath: await chromium.executablePath,
         headless: true,
