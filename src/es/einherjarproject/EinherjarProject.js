ae21b7895af1d7a40c769381256a8a7a4119ad7c
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2021-06-02 09:59:24 +0530

    Rename file

--- src/es/einherjarproject/EinherjarProject.js
+++ src/es/einherjarproject/EinherjarProject.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const einherjarProjectScraper = require("./einherjarProjectScraper");
+const einherjarProjectScraper = require("./EinherjarProjectScraper");
 
 const router = express.Router();
 
