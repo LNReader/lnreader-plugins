da8f94510781bc6677e0a1e09bfd45d40a69cdad
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2021-07-09 14:02:08 +0530

    Add Novel Updates

---
+++ src/en/novelupdates/NovelUpdates.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const novelUpdatesScraper = require("./NovelUpdatesScraper");
+
+// const router = express.Router();
+
+// router.get("/novels/", novelUpdatesScraper.novelsScraper);
+
+// router.get("/novel/:novelUrl", novelUpdatesScraper.novelScraper);
+
+// router.get("/novel/:novelUrl/:chapterUrl", novelUpdatesScraper.chapterScraper);
+
+// router.get("/search/", novelUpdatesScraper.searchScraper);
+
+// module.exports = router;
