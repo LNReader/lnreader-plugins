552219b4bad68da14824e1eddfb9c40dca9402f3
Nyagami <hoangquan05112002@gmail.com>
2023-04-01 03:35:56 +0700

    comment fetch

--- plugins/english/LightNovelReader.js
+++ plugins/english/LightNovelReader.js
@@ -1,4 +1,4 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+// const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const cheerio = require('cheerio');
 const languages = require('@libs/languages');
