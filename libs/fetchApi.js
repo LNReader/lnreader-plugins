c2b7629af26d3721a16ae635b11ce9b0a3987d23
Nyagami <hoangquan05112002@gmail.com>
2023-04-14 01:27:19 +0700

    add novel Fullme

--- libs/fetchApi.js
+++ libs/fetchApi.js
@@ -1,6 +1,6 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-  const defaultUserAgentString =
+const defaultUserAgentString =
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36';
 
 async function fetchApi (url, init, pluginId){
