f9077b0ebcdeef974e53a61a6fd6f479e4c1e84b
Nyagami <hoangquan05112002@gmail.com>
2023-04-13 11:47:34 +0700

    remove try catch

--- api/plugins.js
+++ api/plugins.js
@@ -8,83 +8,46 @@ const githubIconSuffix = '?raw=true';
 const githubPluginsLink = 'https://raw.githubusercontent.com/nyagami/plugins/main/plugins';
 const githubPluginSuffix = '?newtest=true';
 
-const jsonPath = path.join(root, 'plugins', 'plugins.json');
-const jsonMinPath = path.join(root, 'plugins', 'plugins.min.json');
-
 const all_plugins = () => {
     const res = {}
     for (let language in languages) {     //language with English name
         const langPath = path.join(root, 'plugins', language.toLowerCase());
         if (!fs.existsSync(langPath)) continue;
-        try {
-            const plugins = fs.readdirSync(langPath);
-            res[languages[language]] = [];
-            plugins.forEach(plugin => {
-                if(plugin.startsWith('.')) return;
-                const requirePath = `@plugins/${language.toLowerCase()}/${plugin.split('.')[0]}`
-                const instance = require(requirePath);
-                const { id, name, lang, version, icon, description } = instance;
-                const info = { id, name, lang, version, description } // lang: language with native name
-                info.url = `${githubPluginsLink}/${language.toLowerCase()}/${plugin}${githubPluginSuffix}`;
-                info.iconUrl = `${githubIconsLink}/${icon}${githubIconSuffix}`;
-                info.requirePath = requirePath;
-                res[lang].push(info);
-            });
-        } catch (e) {
-            console.log(e);
-        }
+        const plugins = fs.readdirSync(langPath);
+        res[languages[language]] = [];
+        plugins.forEach(plugin => {
+            if(plugin.startsWith('.')) return;
+            const requirePath = `@plugins/${language.toLowerCase()}/${plugin.split('.')[0]}`
+            const instance = require(requirePath);
+            const { id, name, lang, version, icon, description } = instance;
+            const info = { id, name, lang, version, description } // lang: language with native name
+            info.url = `${githubPluginsLink}/${language.toLowerCase()}/${plugin}${githubPluginSuffix}`;
+            info.iconUrl = `${githubIconsLink}/${icon}${githubIconSuffix}`;
+            info.requirePath = requirePath;
+            res[lang].push(info);
+        });
     }
     return res;
 }
 
-const popularNovels = async (pluginRequirePath) => {
-    try{
-        return await require(pluginRequirePath).popularNovels(1);
-    }catch(error){
-        return {
-            "error": error
-        }
-    }
+const popularNovels = async (pluginRequirePath, {showLatestNovels, filters}) => {
+    return await require(pluginRequirePath).popularNovels(1, {showLatestNovels, filters});
 }
 
 const searchNovels = async (pluginRequirePath, searchTerm) => {
-    try{
-        return await require(pluginRequirePath).searchNovels(searchTerm);
-    }catch(error){
-        return {
-            "error": error
-        }
-    }
+    return await require(pluginRequirePath).searchNovels(searchTerm);
 }
 
 const parseNovelAndChapters = async(pluginRequirePath, novelUrl) => {
-    try{
-        return await require(pluginRequirePath).parseNovelAndChapters(novelUrl);
-    }catch(error){
-        return {
-            "error": error
-        }
-    }
+    return await require(pluginRequirePath).parseNovelAndChapters(novelUrl);
 }
 
 const parseChapter = async (pluginRequirePath, chapterUrl) => {
-    try{
-        return await require(pluginRequirePath).parseChapter(chapterUrl);
-    }catch(error){
-        return {
-            "error": error
-        }
-    }
+    return await require(pluginRequirePath).parseChapter(chapterUrl);
 }
 
 const fetchImage = async (pluginRequirePath, url) => {
-    try{
-        return await require(pluginRequirePath).fetchImage(url);
-    }catch(error){
-        return {
-            "error": error
-        }
-    }
+    return await require(pluginRequirePath).fetchImage(url);
 }
 
 module.exports = {
---
+++ libs/filterInputs.js
@@ -0,0 +1,8 @@
+let FilterInputs = /*#__PURE__*/function (FilterInputs) {
+    FilterInputs[FilterInputs["TextInput"] = 0] = "TextInput";
+    FilterInputs[FilterInputs["Picker"] = 1] = "Picker";
+    FilterInputs[FilterInputs["Checkbox"] = 2] = "Checkbox";
+    return FilterInputs;
+}({});
+
+module.exports = FilterInputs
