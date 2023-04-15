503ce6bc5a2003563de47e0f2cb62e2d744d80f9
Nyagami <hoangquan05112002@gmail.com>
2023-04-15 13:35:10 +0700

    remove try catch

--- api/json_plugins.js
+++ api/json_plugins.js
@@ -18,27 +18,22 @@ const json_plugins = () => {
     for (let language in languages) {     // language with English name
         const langPath = path.join(root, 'plugins', language.toLowerCase());
         if (!fs.existsSync(langPath)) continue;
-        try {
-            const plugins = fs.readdirSync(langPath);
-            json[languages[language]] = [];
-            plugins.forEach(plugin => {
-                if(plugin.startsWith('.')) return;
+        const plugins = fs.readdirSync(langPath);
+        json[languages[language]] = [];
+        plugins.forEach(plugin => {
+            if(plugin.startsWith('.')) return;
+            const instance = require(`@plugins/${language.toLowerCase()}/${plugin.split('.')[0]}`);
 
-                const instance = require(`@plugins/${language.toLowerCase()}/${plugin.split('.')[0]}`);
+            const { id, name, site, lang, version, icon, description } = instance;
+            // lang: language with native name
+            const info = { id, name, site, lang, version, description }
+            info.url = `${githubPluginsLink}/${language.toLowerCase()}/${plugin}`;
+            info.iconUrl = `${githubIconsLink}/${icon}`;
 
-                const { id, name, site, lang, version, icon, description } = instance;
-                // lang: language with native name
-                const info = { id, name, site, lang, version, description }
-                info.url = `${githubPluginsLink}/${language.toLowerCase()}/${plugin}`;
-                info.iconUrl = `${githubIconsLink}/${icon}`;
+            json[lang].push(info);
 
-                json[lang].push(info);
-
-                console.log('Collected', name);
-            });
-        } catch (e) {
-            console.log(e);
-        }
+            console.log('Collected', name);
+        });
     }
     fs.writeFileSync(jsonMinPath, JSON.stringify(json));
     fs.writeFileSync(jsonPath, JSON.stringify(json, null, '\t'));
