a6f51e2f46b53f15616b6e4fc390d135f5abe524
Nyagami <hoangquan05112002@gmail.com>
2023-07-02 16:32:04 +0700

    remove ./js (again)

--- js/libs/defaultCover.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = "https://github.com/LNReader/lnreader-sources/blob/main/icons/src/coverNotAvailable.jpg?raw=true";
