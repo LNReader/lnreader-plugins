210ca2fe6e9d004403d447a8e9fbe754c67d1aee
nyagami <hoangquan05112002@gmail.com>
2024-02-08 00:26:40 +0700

    fix: json branch

--- scripts/json_plugins.ts
+++ scripts/json_plugins.ts
@@ -11,7 +11,7 @@ const { execSync } = require("child_process");
 const REMOTE = execSync("git remote get-url origin")
   .toString()
   .replace(/[\s\n]/g, "");
-let CURRENT_BRANCH = 'dist';
+let CURRENT_BRANCH = 'beta-dist';
 try {
   CURRENT_BRANCH = execSync("git rev-parse --abbrev-ref HEAD")
     .toString()
