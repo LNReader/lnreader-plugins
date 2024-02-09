b7cad9e70b7977ef98b5eb81084c64f90f511216
error7404 <48163201+error7404@users.noreply.github.com>
2024-02-09 01:49:38 +0100

    wpmangastream:  - added filter support & showLatestNovels  - added Error on !response.ok  - added Error on Recaptcha on page  - better status finding  - fix \n in summary instead on an actual new line  - fix chapter number and name beeing squiched LightNovelFR: added filters KolNovel: added filters

--- scripts/multisrc/wpmangastream/template.ts
+++ scripts/multisrc/wpmangastream/template.ts
@@ -1,7 +1,9 @@
-import { CheerioAPI, load } from "cheerio";
+import { load } from "cheerio";
 import { fetchApi, fetchFile } from "@libs/fetch";
 import { Plugin } from "@typings/plugin";
 import { NovelStatus } from "@libs/novelStatus";
+import { defaultCover } from "@libs/defaultCover";
+import { Filters } from "@libs/filterInputs";
 
 interface WPmangaStreamOptions {
   reverseChapters?: boolean;
@@ -13,6 +15,7 @@ export interface WPmangaStreamMetadata {
   sourceSite: string;
   sourceName: string;
   options?: WPmangaStreamOptions;
+  filters?: any;
 }
 
 class WPmangaStreamPlugin implements Plugin.PluginBase {
@@ -22,50 +25,82 @@ class WPmangaStreamPlugin implements Plugin.PluginBase {
   site: string;
   version: string;
   options?: WPmangaStreamOptions;
+  filters?: Filters;
 
   constructor(metadata: WPmangaStreamMetadata) {
     this.id = metadata.id;
     this.name = metadata.sourceName;
     this.icon = `multisrc/wpmangastream/icons/${metadata.id}.png`;
     this.site = metadata.sourceSite;
-    this.version = "1.0.0";
+    this.version = "1.0.1";
     this.options = metadata.options;
+    this.filters = metadata.filters satisfies Filters;
   }
 
-  async popularNovels(page: number): Promise<Plugin.NovelItem[]> {
-    const url = this.site + "series/?page=" + page + "&status=&order=popular";
-    const body = await fetchApi(url).then((res) => res.text());
+  async popularNovels(page: number, { filters, showLatestNovels }: Plugin.PopularNovelsOptions): Promise<Plugin.NovelItem[]> {
+    let url = this.site + "series/?page=" + page;
+    if (!filters) filters = {};
+    if (showLatestNovels) url += "&order=latest";
+    for (const key in filters) {
+      if (typeof filters[key].value === "object")
+        for (const value of filters[key].value as string[])
+          url += `&${key}=${value}`;
+      else if (filters[key].value) url += `&${key}=${filters[key].value}`;
+    }
+    const response = await fetchApi(url);
+    if (!response.ok) throw new Error("You got banned ? (check in webview)");
+    const body = await response.text();
     const loadedCheerio = load(body);
+    if (loadedCheerio("title").text().trim() == "Bot Verification")
+      throw new Error("Captcha error, please open in webview");
+
     const novels: Plugin.NovelItem[] = [];
 
     loadedCheerio("article.maindet").each(function () {
-      const name = loadedCheerio(this).find("h2").text();
+      const novelName = loadedCheerio(this).find("h2").text();
       const image = loadedCheerio(this).find("img");
-      const url = loadedCheerio(this).find("h2 a").attr("href");
+      const novelUrl = loadedCheerio(this).find("h2 a").attr("href");
 
-      if (!url) return;
-      novels.push({
-        name,
-        cover: image.attr("data-src") || image.attr("src"),
-        url,
-      });
+      if (novelUrl) {
+        novels.push({
+          name: novelName,
+          cover: image.attr("data-src") || image.attr("src") || defaultCover,
+          url: novelUrl,
+        });
+      }
     });
 
     return novels;
   }
 
   async parseNovelAndChapters(url: string): Promise<Plugin.SourceNovel> {
-    const body = await fetchApi(url).then((res) => res.text());
+    const response = await fetchApi(url);
+    if (!response.ok) throw new Error("You got banned ? (check in webview)");
+    const body = await response.text();
     const loadedCheerio = load(body);
+    if (loadedCheerio("title").text().trim() == "Bot Verification")
+      throw new Error("Captcha error, please open in webview");
 
     const novel: Plugin.SourceNovel = { url };
 
-    // novel.url = url;
     novel.name = loadedCheerio("h1.entry-title").text();
     novel.cover =
       loadedCheerio("img.wp-post-image").attr("data-src") ||
-      loadedCheerio("img.wp-post-image").attr("src");
-    novel.status = loadedCheerio("div.sertostat > span").attr("class");
+      loadedCheerio("img.wp-post-image").attr("src") || defaultCover;
+    switch (loadedCheerio("div.sertostat > span").attr("class")?.toLowerCase() || "") {
+      case "completed":
+        novel.status = NovelStatus.Completed;
+        break;
+      case "ongoing":
+        novel.status = NovelStatus.Ongoing;
+        break;
+      case "hiatus":
+        novel.status = NovelStatus.OnHiatus;
+        break;
+      default:
+        novel.status = NovelStatus.Unknown;
+        break;
+    }
 
     loadedCheerio("div.serl > span").each(function () {
       const detailName = loadedCheerio(this).text().trim();
@@ -90,14 +125,14 @@ class WPmangaStreamPlugin implements Plugin.PluginBase {
     novel.summary = "";
     for (let i = 0; i < summary.length; i++) {
       const p = summary[i];
-      novel.summary += loadedCheerio(p).text().trim() + "\\n\\n";
+      novel.summary += loadedCheerio(p).text().trim() + "\n\n";
     }
 
     const totalChapters = loadedCheerio(".eplister li").length;
     const chapters: Plugin.ChapterItem[] = loadedCheerio(".eplister li")
       .map((chapterIndex, element) => ({
         name:
-          loadedCheerio(element).find(".epl-num").text() +
+          loadedCheerio(element).find(".epl-num").text() + " " + 
           loadedCheerio(element).find(".epl-title").text(),
         url: loadedCheerio(element).find("a").attr("href") || "",
         releaseTime: loadedCheerio(element).find(".epl-date").text().trim(),
@@ -115,31 +150,41 @@ class WPmangaStreamPlugin implements Plugin.PluginBase {
   }
 
   async parseChapter(chapterUrl: string): Promise<string> {
-    const body = await fetchApi(chapterUrl).then((res) => res.text());
+    const response = await fetchApi(chapterUrl);
+    if (!response.ok) throw new Error("You got banned ? (check in webview)");
+    const body = await response.text();
     const loadedCheerio = load(body);
+    if (loadedCheerio("title").text().trim() == "Bot Verification")
+      throw new Error("Captcha error, please open in webview");
 
     const chapterText = loadedCheerio(".epcontent").html() || "";
     return chapterText;
   }
 
-  async searchNovels(searchTerm: string): Promise<Plugin.NovelItem[]> {
+  async searchNovels(searchTerm: string, page: number): Promise<Plugin.NovelItem[]> {
+    if (page != 1) return [];
     const url = this.site + "?s=" + searchTerm;
-    const body = await fetchApi(url).then((res) => res.text());
+    const response = await fetchApi(url);
+    if (!response.ok) throw new Error("You got banned ? (check in webview)");
+    const body = await response.text();
     const loadedCheerio = load(body);
+    if (loadedCheerio("title").text().trim() == "Bot Verification")
+      throw new Error("Captcha error, please open in webview");
 
     const novels: Plugin.NovelItem[] = [];
 
     loadedCheerio("article.maindet").each(function () {
-      const name = loadedCheerio(this).find("h2").text();
+      const novelName = loadedCheerio(this).find("h2").text();
       const image = loadedCheerio(this).find("img");
-      const url = loadedCheerio(this).find("h2 a").attr("href");
+      const novelUrl = loadedCheerio(this).find("h2 a").attr("href");
 
-      if (!url) return;
-      novels.push({
-        name,
-        cover: image.attr("data-src") || image.attr("src"),
-        url,
-      });
+      if (novelUrl){
+        novels.push({
+          name: novelName,
+          cover: image.attr("data-src") || image.attr("src") || defaultCover,
+          url: novelUrl,
+        });
+      }
     });
 
     return novels;
