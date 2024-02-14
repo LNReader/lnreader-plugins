6d52063ebfee0ff17b847ca6cd8db6759c8ff5f9
error7404 <48163201+error7404@users.noreply.github.com>
2024-02-14 10:16:00 +0100

    Added README.md to lightnovelwp

--- scripts/multisrc/lightnovelwp/generator.ts
+++ scripts/multisrc/lightnovelwp/generator.ts
@@ -1,37 +1,3 @@
-/**
- * This generator is for most sites that uses the LightNovel WordPress Theme:
- * https://themesia.com/lightnovel-wordpress-theme/
- * 
- * it should work for all sites that uses this theme (highest version tested: 1.1.5)
- * to know the version of the theme you can add "/wp-content/themes/lightnovel/style.css"
- * to the site url and check the version in the file
- * or you can check the version by entering the url in : "https://www.wpthemedetector.com"
- * (it sometimes hallucinates for version 1.0.X as 1.0)
- */
-
-
-/** =========================ADD A NEW SOURCE=========================
- * To add a new source you need to add it to sources.json:
- * - id: the id of the source (something unique)
- * - sourceName: the name of the source (the name that will be displayed in the app)
- * - sourceSite: the site url
- * - options: the options of the source
- *  - lang: the language of the source (default: "English") (check that the language
- *    exists in the languages (check folder names in "plugins/"))
- *  - reverseChapter: if the chapters are in reverse order
- *    (if the first chapter of the list is 1 set it to true)
- * 
- * To add an icon to the source you need to find the icon of the site
- * (try the favicon of the site (https://site.com/favicon.ico)
- * (don't forget to convert it to png))
- * and add it to the folder "icons/multisrc/lightnovelwp" named as the id of the source
- * 
- * To add filters to a source you need to run the script "get_filters.ts"
- * (./node_modules/.bin/ts-node scripts/multisrc/lightnovelwp/get_filters.ts
- * (if you are in the root of the project) (and you have run "npm install" before))
- * and follow the instructions (url is easier and faster but sometimes it doesn't work)
- */
-
 import { ScrpitGeneratorFunction } from "../generate";
 import { LightNovelWPMetadata } from "./template";
 import list from "./sources.json";
