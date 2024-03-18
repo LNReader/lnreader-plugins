c5ed1bf52c95762395f8e3628e57ec3057c0a088
nyagami <hoangquan05112002@gmail.com>
2024-03-18 23:32:05 +0700

    add total.svg to dist branch

--- host.sh
+++ host.sh
@@ -20,7 +20,7 @@ rm -rf .js
 npm run clearMultisrc
 npm run generate
 npm run json
-git add -f icons .dist .js/plugins
+git add -f icons .dist .js/plugins total.svg
 git commit -m "Host plugins"
 git push -f origin $dist
 
