b4883d9d54c324106282875a6a3b1c9ad630153b
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2024-04-03 09:07:25 +0530

    Early Exit if Node Version is Lower

--- tswatch.ts
+++ tswatch.ts
@@ -1,18 +1,25 @@
 import { TscWatchClient } from 'tsc-watch/client';
 
+const nodeVersion = Number(process.versions.node.split('.')[0]);
+
+if (nodeVersion < 18) {
+  console.error(
+    `\nYou are using Node.js ${process.versions.node}. For lnreader-plugins, Node.js version >= v18.0.0 is required.`,
+  );
+
+  process.exit(1);
+}
+
 const watch = new TscWatchClient();
 
 let port = Number(process.argv[2]);
+
 if (!port) {
-  console.log(`Port not specified! Setting default 3000!
-Run \`npm start -- <port>\` to change it
-`);
+  console.log(
+    `No port specified; defaulting to 3000. Change it with 'npm start -- <PORT>'.`,
+  );
+
   port = 3000;
 }
 
 watch.start('--onSuccess', `node ./.js/index.js ${port}`);
-
-try {
-} catch (e) {
-  watch.kill(); // Fatal error, kill the compiler instance.
-}
