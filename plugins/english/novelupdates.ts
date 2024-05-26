e01770ddab68d31b21449cf05dcd3c0884ad00d8
Batorian <lukas.richter.berlin@web.de>
2024-05-26 22:41:41 +0200

    Fix: Novel World Translations cleaned chapterContent

--- plugins/english/novelupdates.ts
+++ plugins/english/novelupdates.ts
@@ -319,9 +319,8 @@ class NovelUpdates implements Plugin.PluginBase {
         }
         break;
       case 'novelworldtranslations':
-        bloatClasses = ['.separator span'];
+        bloatClasses = ['.separator img'];
         bloatClasses.map(tag => loadedCheerio(tag).remove());
-        loadedCheerio('.entry-content div div p span').last().remove();
         chapterTitle = loadedCheerio('.entry-title').first().text()!;
         chapterContent = loadedCheerio('.entry-content')
           .html()!
