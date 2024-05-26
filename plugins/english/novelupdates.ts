efd7c015db8e91db16b1943913c352175121aafd
Batorian <lukas.richter.berlin@web.de>
2024-05-26 12:47:06 +0200

    Enhancement: chapterContent

--- plugins/english/novelupdates.ts
+++ plugins/english/novelupdates.ts
@@ -343,10 +343,7 @@ class NovelUpdates implements Plugin.PluginBase {
           throw new Error('Age verification required, please open in webview.');
         }
         chapterTitle = `${loadedCheerio('.pl-4 h1').first().text()!} | ${loadedCheerio('.pl-4 div').first().text()!}`;
-        chapterContent = loadedCheerio('#startContainer')
-          .siblings()
-          .first()
-          .children()
+        chapterContent = loadedCheerio('#startContainer + * > *')
           .first()
           .html()!;
         if (chapterTitle && chapterContent) {
