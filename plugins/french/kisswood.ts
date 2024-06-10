53a143193aeed5b7b727674a1297c4e257bc40ca
Elfing <106489440+elfing44@users.noreply.github.com>
2024-06-10 07:14:57 +0200

    Update plugins/french/kisswood.ts
    Co-authored-by: nyagami <hoangquan05112002@gmail.com>

--- plugins/french/kisswood.ts
+++ plugins/french/kisswood.ts
@@ -247,11 +247,5 @@ class KissWoodPlugin implements Plugin.PluginBase {
     return novels;
   }
 
-  async fetchImage(url: string): Promise<string | undefined> {
-    // if your plugin has images and they won't load
-    // this is the function to fiddle with
-    return fetchFile(url);
-  }
-}
 
 export default new KissWoodPlugin();
