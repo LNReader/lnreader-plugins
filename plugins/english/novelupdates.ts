1a23af4d85ee576ff38e6d6ba73442e5e9829464
Batorian <lukas.richter.berlin@web.de>
2024-06-11 22:33:00 +0200

    Fix: updated versions

--- plugins/english/novelupdates.ts
+++ plugins/english/novelupdates.ts
@@ -6,7 +6,7 @@ import { Plugin } from '@typings/plugin';
 class NovelUpdates implements Plugin.PluginBase {
   id = 'novelupdates';
   name = 'Novel Updates';
-  version = '0.5.6';
+  version = '0.5.7';
   icon = 'src/en/novelupdates/icon.png';
   site = 'https://www.novelupdates.com/';
 
