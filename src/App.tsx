b214739cb4f99cb8c2afe9ebb24f5e20c64748e7
nyagami <hoangquan05112002@gmail.com>
2024-06-14 16:59:26 +0700

    simple examples for set cookies and fetch request

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,6 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    fetch('https://google.com')
-      .then(res => res.text())
-      .then(data => console.log(data));
-  }, []);
   const [count, setCount] = useState(0);
 
   return (
@@ -23,8 +18,34 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
+        <button
+          onClick={() => {
+            setCount(count => count + 1);
+            fetch('https://google.com', {
+              headers: {
+                'User-Agent':
+                  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+              },
+            })
+              .then(res => res.text())
+              .then(data => console.log(data));
+          }}
+        >
           count is {count}
+        </button>
+        <button
+          onClick={() => {
+            fetch('cookies', {
+              method: 'post',
+              body: `This is my cookie, Do you want to eat?`,
+            }).then(res => {
+              if (res.ok) {
+                console.log('got cookies');
+              }
+            });
+          }}
+        >
+          Set Cookie
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
