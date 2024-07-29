9373e751bc886f3e9a024429c611764c93d47cf1
Dredd1987 <aliahmohammed1918@gmail.com>
2024-07-29 06:58:23 +0300

    fix handling single chapter stories

--- src/plugins/english/ao3.ts
+++ src/plugins/english/ao3.ts
@@ -144,22 +144,40 @@ class ArchiveOfOurOwn implements Plugin.PluginBase {
       .join(',');
     novel.summary = `Fandom:\n${fandom}\n\nRating:\n${rating}\n\nWarning:\n${warning}\n\nSummary:\n${summary}\n\nSeries:\n${series}\n\nRelationships:\n${relation}\n\nCharacters:\n${character}\n\nStats:\n${stats}`;
     const chapterItems: Plugin.ChapterItem[] = [];
-    loadedCheerio('#chapter_index select').each((i, selectEl) => {
-      loadedCheerio(selectEl)
-        .find('option')
-        .each((i, el) => {
-          const chapterName = loadedCheerio(el).text().trim();
-          const chapterUrlCode = loadedCheerio(el).attr('value')?.trim();
-          const chapterUrl = `${novelUrl}/chapters/${chapterUrlCode}`;
-
-          if (chapterUrl) {
-            chapterItems.push({
-              name: chapterName,
-              path: new URL(chapterUrl, this.site).toString(),
-            });
-          }
-        });
-    });
+    if (loadedCheerio('#chapter_index select').length > 0) {
+      loadedCheerio('#chapter_index select').each((i, selectEl) => {
+        loadedCheerio(selectEl)
+          .find('option')
+          .each((i, el) => {
+            const chapterName = loadedCheerio(el).text().trim();
+            const chapterUrlCode = loadedCheerio(el).attr('value')?.trim();
+            const chapterUrl = `${novelUrl}/chapters/${chapterUrlCode}`;
+    
+            if (chapterUrl) {
+              chapterItems.push({
+                name: chapterName,
+                path: new URL(chapterUrl, this.site).toString(),
+              });
+            }
+          });
+      });
+    } else {
+      loadedCheerio('div#chapters h3.title').each((i, titleEl) => {
+        const fullTitleText = loadedCheerio(titleEl).text().trim();
+        const chapterNameMatch = fullTitleText.match(/: "(.*)"/);
+        const chapterName = chapterNameMatch ? chapterNameMatch[1] : '';
+        const chapterUrlRaw = loadedCheerio(titleEl).find('a').attr('href')?.trim();
+        const chapterUrlCode = chapterUrlRaw?.match(/\/chapters\/(\d+)/)?.[1];
+        const chapterUrl = `${novelUrl}/chapters/${chapterUrlCode}`;
+    
+        if (chapterUrl) {
+          chapterItems.push({
+            name: chapterName,
+            path: new URL(chapterUrl, this.site).toString(),
+          });
+        }
+      });
+    }
     novel.chapters = chapterItems;
 
     return novel;
