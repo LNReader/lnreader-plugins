ec60efa342f11c897ac45ad5b4cc458c4f3c3000
Dredd1987 <aliahmohammed1918@gmail.com>
2024-08-27 02:16:05 +0300

    fix test newsource

--- src/plugins/arabic/dilartube.ts
+++ src/plugins/arabic/dilartube.ts
@@ -4,32 +4,66 @@ import { Plugin } from '@typings/plugin';
 import { Filters, FilterTypes } from '@libs/filterInputs';
 import { defaultCover } from '@libs/defaultCover';
 
-class Sunovels implements Plugin.PagePlugin {
+import crypto from 'crypto';
+
+// Decrypt function
+function decrypt(responseData: string): string {
+  const enc = responseData.split('|');
+
+  // Helper function to compute SHA-256 hash
+  const sha256 = (input: string): string => {
+      return crypto.createHash('sha256').update(input, 'utf8').digest('hex');
+  };
+
+  // Convert hex string to byte array
+  const hexStringToByteArray = (hexString: string): Uint8Array => {
+      const length = hexString.length;
+      const byteArray = new Uint8Array(length / 2);
+      for (let i = 0; i < length; i += 2) {
+          byteArray[i / 2] = parseInt(hexString.substr(i, 2), 16);
+      }
+      return byteArray;
+  };
+
+  // AES decryption
+  const aesDecrypt = (encryptedText: string, secretKey: Uint8Array, ivString: string): string => {
+      const decipher = crypto.createDecipheriv(
+          'aes-256-cbc',
+          secretKey,
+          Buffer.from(ivString, 'base64')
+      );
+      let decrypted = decipher.update(encryptedText, 'base64', 'utf8');
+      decrypted += decipher.final('utf8');
+      return decrypted;
+  };
+
+  // Generate the secret key
+  const secretKey = hexStringToByteArray(sha256(enc[3]));
+
+  // Decrypt the message
+  return aesDecrypt(enc[0], secretKey, enc[2]);
+}
+
+class Sunovels implements Plugin.PluginBase {
   id = 'dilartube';
   name = 'dilar tube';
   version = '1.0.0';
   icon = 'src/ar/sunovels/icon.png';
   site = 'https://dilar.tube/';
 
-  parseNovels(loadedCheerio: CheerioAPI): Plugin.NovelItem[] {
+  parseNovels(data: ApiResponse): Plugin.NovelItem[] {
     const novels: Plugin.NovelItem[] = [];
-    loadedCheerio('body div.link').each((idx, ele) => {
-      loadedCheerio(ele)
-        .find('div.card')
-        .each((idx, ele) => {
-          const novelName = loadedCheerio(ele).find('div.header').text().trim();
-          const novelUrl =
-            loadedCheerio(ele).find('a').attr('href')?.trim().slice(1);
-          let novelCover = 
-          loadedCheerio(ele).find('img').attr('src')?.trim();
-          const novel = {
-            name: novelName,
-            cover: novelCover,
-            path: novelUrl,
-          };
-        });
-    });
+    data.releases
+    .filter((release) => release.manga.is_novel)  // Filter for novels only
+    .map((release) => {
+      const manga = release.manga;
 
+      novels.push({
+        name: manga.title,  // Extract the novel's title
+        path: `api/mangas/${manga.id}`,  // Construct the path, using id or slug if available
+        cover: `uploads/manga/cover/3543/${manga.cover}`,  // Construct the full cover URL
+      });
+    });
     return novels;
   }
 
@@ -37,9 +71,10 @@ class Sunovels implements Plugin.PagePlugin {
     page: number,
     { showLatestNovels, filters }: Plugin.PopularNovelsOptions<Filters>,
   ): Promise<Plugin.NovelItem[]> {
-    const pageCorrected = page - 1;
-    let link = `${this.site}mangas`;
-
+    let link = `${this.site}releases`;
+    if (showLatestNovels){
+      link = `${this.site}releases`;
+    }
     // if (filters) {
     //   if (
     //     Array.isArray(filters.categories.value) &&
@@ -54,63 +89,58 @@ class Sunovels implements Plugin.PagePlugin {
     //   }
     // }
     // link += `&page=${pageCorrected}`;
-    const body = await fetchApi(link).then(r => r.text());
-    const loadedCheerio = parseHTML(body);
-    return this.parseNovels(loadedCheerio);
+    // const body = await fetchApi(link).then(r => r.text());
+    const response = await fetchApi(link).then(r => r.json());
+    console.log(response)
+    return this.parseNovels(response);
   }
 
-  async parseNovel(
-    novelUrl: string,
-  ): Promise<Plugin.SourceNovel & { totalPages: number }> {
-    const result = await fetchApi(new URL(novelUrl, this.site).toString());
-    const body = await result.text();
-    const loadedCheerio = parseHTML(body);
-
+  async parseNovel(novelUrl: string): Promise<Plugin.SourceNovel> {
+    const chapterItems: Plugin.ChapterItem[] = [];
+    let fullUrl = this.site + novelUrl;
+    let chapterUrl = this.site + novelUrl + '/releases'
+    const manga = await fetchApi(fullUrl).then(r => r.json());
+    const chapters = await fetchApi(chapterUrl).then(r => r.json());
+    const mangaData = manga.mangaData;
+    const chapterData = chapters.releases
+  
     const novel: Plugin.SourceNovel & { totalPages: number } = {
       path: novelUrl,
-      name: loadedCheerio('div.main-head h3').text().trim() || 'Untitled',
-      author: loadedCheerio('.novel-author').text().trim(),
-      summary: loadedCheerio('section.info-section div.description p')
-        .text()
-        .trim(),
+      name: mangaData.arabic_title || 'Untitled',
+      author: (mangaData.authors.length > 0 ? mangaData.authors[0].name : '') || 'Unknown',
+      summary: mangaData.summary || '',
+      cover: `${this.site}uploads/manga/cover/${mangaData.id}/${mangaData.cover}`,
       totalPages: 1,
       chapters: [],
     };
+  
+    // Status and genres
+    const translationStatusId:string = mangaData.status.toString();
+    const translationText = {
+      '1': 'مستمره',
+      '0': 'منتهية',
+      '2': 'متوقفة',
+      '3': 'غير مترجمه',
+    }[translationStatusId] || 'غير معروف';
     const statusWords = new Set(['مكتمل', 'جديد', 'مستمر']);
-    const mainGenres = Array.from(loadedCheerio('div.categories li.tag'))
-      .map(el => loadedCheerio(el).text().trim())
-      .join(',');
-    const statusGenre = Array.from(
-      loadedCheerio('div.header-stats span').eq(3).find('strong'),
-    )
-      .map(el => loadedCheerio(el).text().trim())
-      .filter(text => statusWords.has(text));
-    novel.genres = `${statusGenre},${mainGenres}`;
-    const statusText = Array.from(
-      loadedCheerio('div.header-stats span').eq(3).find('strong'),
-    )
-      .map(el => loadedCheerio(el).text().trim())
-      .filter(text => statusWords.has(text))
-      .join();
-    novel.status =
-      {
-        'جديد': 'Ongoing',
-        'مكتمل': 'Completed',
-        'مستمر': 'Ongoing',
-      }[statusText] || 'Unknown';
-    const imageUrl = loadedCheerio('div.img-container figure.cover img').attr(
-      'src',
-    );
-    const imageUrlFull = this.site + imageUrl?.slice(1);
-    novel.cover = imageUrlFull;
-    const chapterNumberStr = loadedCheerio('div.header-stats span')
-      .first()
-      .text()
-      .replace(/[^\d]/g, '');
-    const chapterNumber = parseInt(chapterNumberStr, 10);
-    const pageNumber = Math.ceil(chapterNumber / 50);
-    novel.totalPages = pageNumber;
-
+    const mainGenres = mangaData.categories.map(category => category.name).join(',');
+    novel.genres = `${translationText},${mainGenres}`;
+    
+    const statusId:string = mangaData.status.toString();
+    const statusText = {
+      '2': 'Ongoing',
+      '3': 'Completed',
+    }[statusId] || 'Unknown';
+    novel.status = statusText
+    chapters.map((item: ChapterRelease) => {
+      chapters.push({
+        name: item.title,
+        releaseTime: new Date(item.created_at).toISOString(),
+        path: `${novelUrl}/${mangaData.title.replace(' ','-')}/${item.chapter}`,
+        chapterNumber: item.chapter,
+      });
+    });
+    novel.chapters = chapterItems;
     return novel;
   }
   parseChapters(data: { chapters: ChapterEntry[] }) {
@@ -200,50 +230,51 @@ class Sunovels implements Plugin.PagePlugin {
     searchTerm: string,
     page: number
   ): Promise<Plugin.NovelItem[]> {
-    const searchUrl = 'https://dilar.tube/api/mangas/search';
+    const searchUrl = `${this.site}api/mangas/search`;
   
-    // Construct the payload
-    const payload = {
-      title: searchTerm,
-      manga_types: {
-        include: ["1", "2", "3", "4", "5", "6", "7", "8"],
-        exclude: []
-      },
-      oneshot: false,
-      novel: true,
-      story_status: {
-        include: [],
-        exclude: []
-      },
-      translation_status: {
-        include: [],
-        exclude: ["3"]
-      },
-      categories: {
-        include: [null],
-        exclude: []
-      },
-      chapters: {
-        min: "",
-        max: ""
-      },
-      dates: {
-        start: null,
-        end: null
-      },
-      page: page
-    };
-  
-    const result = await fetchApi(searchUrl, {
+    const options = {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(payload)
-    });
-  
-    const body = await result.text();
-    const loadedCheerio = parseHTML(body);
+      'Sec-Fetch-Mode': 'no-cors',
+      body: JSON.stringify({
+        title: `"${searchTerm}"`,
+        manga_types: {
+          include: ["1", "2", "3", "4", "5", "6", "7", "8"],
+          exclude: [],
+        },
+        oneshot: false,
+        novel: false,
+        story_status: {
+          include: [],
+          exclude: [],
+        },
+        translation_status: {
+          include: [],
+          exclude: ["3"],
+        },
+        categories: {
+          include: [null],
+          exclude: [],
+        },
+        chapters: {
+          min: "",
+          max: "",
+        },
+        dates: {
+          start: null,
+          end: null,
+        },
+        page: page,
+      }),
+    };
+    
+    const result = await fetchApi(searchUrl, options).then(response => response.json());
+    const body = await result.json();
+    const decryptedData = decrypt(body.data);
+    const jsonData = JSON.parse(decryptedData);
+    const loadedCheerio = parseHTML(jsonData);
     return this.parseNovels(loadedCheerio);
   }
 
@@ -326,3 +357,249 @@ interface ChapterEntry {
   releaseTime: string;
   chapterNumber: string | number;
 }
+interface Uploader {
+  id: number;
+  nick: string;
+  rev_links: any[];
+  admob_rewarded_tag_ios: string | null;
+  admob_rewarded_tag_android: string | null;
+}
+
+interface Team {
+  id: number;
+  name: string;
+  rating: number;
+  settings: Record<string, unknown>;
+  paypal: string | null;
+}
+
+interface CoverBlurhash {
+  hash: string;
+  width: number;
+  height: number;
+}
+
+interface BannerBlurhash {
+  hash: string;
+  width: number;
+  height: number;
+}
+
+interface Category {
+  id: number;
+  name: string;
+  icon: string | null;
+  manga_id: number;
+}
+
+interface Type {
+  id: number;
+  name: string;
+  reading_direction: string;
+  title: string;
+}
+
+interface Manga {
+  id: number;
+  title: string;
+  summary: string;
+  is_novel: boolean;
+  is_oneshot: boolean;
+  genre_id: number;
+  cover: string;
+  cover_pos: number;
+  rectangle_cover_pos: number;
+  cover_blurhash: CoverBlurhash;
+  banner: string;
+  banner_blurhash: BannerBlurhash;
+  manga_type_id: number;
+  reading_direction: string;
+  story_status: number;
+  translation_status: number;
+  vols: number;
+  chaps: number;
+  reviewed: boolean;
+  banned: boolean;
+  rating: string;
+  rates_count: number;
+  commentable: boolean;
+  show_comments: boolean;
+  deleted_at: string | null;
+  delete_reason: string;
+  time_stamp: number;
+  latest_chapterization_id: number;
+  uniq_visitors_count: number;
+  publisher_id: number | null;
+  publisher_name: string | null;
+  discord_url: string | null;
+  mobile_exclusive: boolean;
+  authors: any[];
+  artists: any[];
+  categories: Category[];
+  type: Type;
+}
+
+interface Release {
+  id: number;
+  manga_id: number;
+  created_at: string;
+  time_stamp: number;
+  views: number;
+  encoded: boolean;
+  showable: boolean;
+  link_control: number;
+  support_link: string;
+  creator_id: number;
+  chapterization_id: number;
+  chapter: number;
+  volume: number;
+  title: string;
+  team_id: number;
+  team_name: string;
+  team_rating: string;
+  team_settings: string;
+  team_paypal: string | null;
+  uploader: Uploader;
+  has_rev_link: boolean;
+  teams: Team[];
+  manga: Manga;
+}
+
+interface ApiResponse {
+  releases: Release[];
+}
+
+type MangaLibrary = {
+  reading: number;
+  completed: number;
+  on_hold: number;
+  dropped: number;
+  planning: number;
+  plan_to_read: number;
+  favorite: number;
+  suggestion: number;
+};
+
+type MangaCategory = {
+  id: number;
+  name: string;
+  icon: string | null;
+  manga_id: number;
+};
+
+type MangaBlurhash = {
+  hash: string;
+  width: number;
+  height: number;
+};
+
+type AuthorArtist = {
+  id: number;
+  name: string;
+  manga_id: number;
+  role: string;
+};
+
+type MangaType = {
+  id: number;
+  name: string;
+  reading_direction: string;
+  title: string;
+};
+
+type MangaData = {
+  id: number;
+  title: string;
+  summary: string;
+  is_novel: boolean;
+  is_oneshot: boolean;
+  genre_id: number;
+  cover: string;
+  cover_pos: number;
+  rectangle_cover_pos: number;
+  cover_blurhash: MangaBlurhash;
+  banner: string;
+  banner_blurhash: MangaBlurhash;
+  manga_type_id: number;
+  reading_direction: string;
+  story_status: number;
+  translation_status: number;
+  vols: number;
+  chaps: number;
+  reviewed: boolean;
+  banned: boolean;
+  rating: string;
+  rates_count: number;
+  commentable: boolean;
+  show_comments: boolean;
+  deleted_at: string | null;
+  delete_reason: string | null;
+  time_stamp: number;
+  latest_chapterization_id: number;
+  uniq_visitors_count: number;
+  publisher_id: number | null;
+  publisher_name: string | null;
+  arabic_title: string;
+  english: string;
+  synonyms: string;
+  japanese: string;
+  over17: boolean;
+  s_date: string;
+  e_date: string | null;
+  mal: string;
+  mangaupdates: string;
+  mangadex: string | null;
+  anilist: string;
+  official_site: string | null;
+  manga_fox: string | null;
+  wikia: string | null;
+  mangarock_oid: string | null;
+  comicvine_id: string | null;
+  external_source: string | null;
+  discord_url: string | null;
+  discord_id: string | null;
+  anime_start_end: string | null;
+  creator_id: number;
+  creator_nick: string;
+  editor_id: number;
+  editor_nick: string;
+  mobile_exclusive: boolean;
+  authors: AuthorArtist[];
+  artists: AuthorArtist[];
+  categories: MangaCategory[];
+  type: MangaType;
+  memberRating: number;
+};
+
+type MangaResponse = {
+  mangaLibrary: MangaLibrary;
+  membersMentioning: any[];
+  memberRates: any | null;
+  mangaLogs: Record<string, any>;
+  mangaData: MangaData;
+};
+type ChapterRelease = {
+  id: number;
+  manga_id: number;
+  created_at: string; // ISO 8601 date string
+  time_stamp: number;
+  views: number;
+  link_control: number;
+  support_link: string;
+  init_team: number;
+  creator_id: number;
+  chapterization_id: number;
+  chapterization_time_stamp: number;
+  chapter: number;
+  volume: number;
+  title: string;
+  team_id: number;
+  team_name: string;
+  uploader: Uploader;
+  has_rev_link: boolean;
+  teams: Team[];
+};
+
+type ChapterResponse = {
+  releases: ChapterRelease[];
+};
