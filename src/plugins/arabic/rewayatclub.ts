2c10b107c57f4726ef6decf4256ea196f9ea5c0c
Dredd1987 <aliahmohammed1918@gmail.com>
2024-09-03 13:41:44 +0300

    Rewayat

--- src/plugins/arabic/rewayatclub.ts
+++ src/plugins/arabic/rewayatclub.ts
@@ -7,7 +7,7 @@ import { defaultCover } from '@libs/defaultCover';
 class RewayatClub implements Plugin.PagePlugin {
   id = 'rewayatclub';
   name = 'Rewayat Club';
-  version = '1.0.0';
+  version = '1.0.1';
   icon = 'src/ar/rewayatclub/icon.png';
   site = 'https://rewayat.club/';
 
