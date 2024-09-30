64df5ad3dc9d9711c743e0a1745de07dbcfd108d
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-09-30 06:01:56 +0200

    lint

--- src/plugins/english/freewebnovel.ts
+++ src/plugins/english/freewebnovel.ts
@@ -120,10 +120,7 @@ class FreeWebNovel implements Plugin.PluginBase {
         /<p>\s*(?:(?:This (?:chapter is updated by|content is taken from)|Follow current novels on) )?(?:[ƒfF][Rrɾг][Eēeё][Eēёe][Wwω][Eёēe][Bbɓ][Nnɳη][Oø૦ѳσo][Vѵv][Eёeē][LlℓɭI\|]\.\s?[Cƈcç][O૦σøoѳ][M๓ɱm]|ꜰʀᴇᴇᴡᴇʙɴᴏᴠᴇʟ)\.?/g,
         '<p>',
       )
-      .replace(
-        /<p>\s*Visit for the best novel reading experience\.?/g,
-        '<p>',
-      );
+      .replace(/<p>\s*Visit for the best novel reading experience\.?/g, '<p>');
   }
 
   async searchNovels(searchTerm: string): Promise<Plugin.NovelItem[]> {
