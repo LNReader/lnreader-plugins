778a5beaed7dcafb39774a86882e5b8389d7669f
Rider21 <58046032+Rider21@users.noreply.github.com>
2024-11-06 17:06:34 +0300

    Dependency optimization (#1294)
    * -qs
    
    * -urlencode
    
    * +urlencode
    
    * aaa

--- src/plugins/english/LibRead.ts
+++ src/plugins/english/LibRead.ts
@@ -1,7 +1,6 @@
 import { Plugin } from '@typings/plugin';
 import { fetchApi } from '@libs/fetch';
 import { CheerioAPI, load as parseHTML } from 'cheerio';
-import qs from 'qs';
 import { Filters, FilterTypes } from '@libs/filterInputs';
 
 class LibReadPlugin implements Plugin.PluginBase {
@@ -131,7 +130,7 @@ class LibReadPlugin implements Plugin.PluginBase {
         Origin: this.site,
       },
       method: 'POST',
-      body: qs.stringify({ searchkey: searchTerm }),
+      body: new URLSearchParams({ searchkey: searchTerm }).toString(),
     });
     if (!r.ok)
       throw new Error(
