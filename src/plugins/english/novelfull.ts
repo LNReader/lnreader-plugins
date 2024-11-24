0826e668d38b7adde0c69639e35134cac5c1cedd
Palloxin <75091899+Palloxin@users.noreply.github.com>
2024-11-24 23:13:16 +0100

    \s

--- src/plugins/english/novelfull.ts
+++ src/plugins/english/novelfull.ts
@@ -120,7 +120,7 @@ class NovelFull implements Plugin.PluginBase {
     const chapterText = loadedCheerio('#chapter-content').html() || '';
 
     return chapterText.replace(
-      /If you find any errors \(Ads popup, ads redirect, broken links, non-standard content, etc\.\.\), Please let us know \S* report chapter \S* so we can fix it as soon as possible\./,
+      /If you find any errors \(\s*Ads popup, ads redirect, broken links, non-standard content, etc\.\.\s*\), Please let us know \S* report chapter \S* so we can fix it as soon as possible\./,
       '',
     );
   }
