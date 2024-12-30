aea4905839acd88b02d35fecf0b55a98d3dcd7ae
cd-z <p_loser_austria@gmx.at>
2024-12-30 14:35:33 +0100

    save keys file and additional safety

--- .github/scripts/createOptions.sh
+++ .github/scripts/createOptions.sh
@@ -1,35 +1,25 @@
-current=`git rev-parse --abbrev-ref HEAD`
 version=`node -e "console.log(require('./package.json').version);"`
 dist="plugins/v$version"
 
-# PLUGIN_JSON=$(curl -s https://raw.githubusercontent.com/LNReader/lnreader-plugins/plugins/v3.0.0/.dist/plugins.min.json | jq '.[] | {(.id): .name}')
-PLUGIN_CSV=$(curl -s https://raw.githubusercontent.com/LNReader/lnreader-plugins/{$dist}/.dist/plugins.min.json | jq '[.[] | {(.name + " Id: " + .id): .id}]' | jq 'reduce .[] as $item ({}; . * $item)')
+PLUGINS=$(curl -s https://raw.githubusercontent.com/LNReader/lnreader-plugins/{$dist}/.dist/plugins.min.json | jq '[.[] | {(.name): .id}]' | jq 'reduce .[] as $item ({}; . * $item)')
 
-# $(echo "$PLUGIN_CSV"|jq  'keys[]' | jq -R -s -c 'split("\n") | map(select(. != ""))')
-KEYS=$(echo $PLUGIN_CSV | jq 'keys_unsorted')
-# echo "$KEYS"
-node -e "const fs = require('fs');const filePath = '.github/scripts/blank_report_issue.yml';const rawText = fs.readFileSync(filePath, 'utf8');const text = $KEYS.join('\"\n        - \"');fs.writeFileSync('.github/ISSUE_TEMPLATE/report_issue.yml', rawText.replace(/{#CHANGE#}/g,'- \"'+ text +'\"'));" 
-# OUTPUT=$(yq -iy '.body[0].type.attributes.options = "$KEYS"' .github/ISSUE_TEMPLATE/report_issue.yml )
+KEYS=$(echo $PLUGINS | jq 'keys_unsorted')
 
-# echo "$OUTPUT" 
-# git checkout --orphan $dist
-
-# if [ $? -eq 1 ]; then
-#     # If checkout failed
-#     echo "=========="
-#     echo "Could not checkout branch dist! See the error above and fix it!"
-#     exit 1
-# fi
-
-# git reset
-# rm -rf .js
-# npm run clearMultisrc
-# npm run generate
-# npx tsc --project tsconfig.production.json
-# npm run json
-# git add -f public/static .dist .js/src/plugins total.svg
-# git commit -m "chore: Publish Plugins"
-# git push -f origin $dist
-
-# git checkout -f $current
-
+node -e "
+    const fs = require('fs');
+    const rawText = fs.readFileSync('.github/scripts/blank_report_issue.yml', 'utf8');
+    const newKeys = $KEYS;
+    let savedKeys = [];
+    try {
+        let keys = JSON.parse(fs.readFileSync('.github/scripts/keys.json', 'utf8'));
+        savedKeys = Object.keys(keys);
+    } catch(err) {console.log(err)}
+    if(!sameKeys(newKeys, savedKeys) && Array.isArray(newKeys)) {
+        const text = newKeys.join('\n        - ');
+        fs.writeFileSync('.github/ISSUE_TEMPLATE/report_issue.yml', rawText.replace(/{#CHANGE#}/g,'- '+ text ));
+        fs.writeFileSync('.github/scripts/keys.json', JSON.stringify($PLUGINS));
+    }
+    function sameKeys(a, b) {
+        return a.length === b.length && a.every(value => b.includes(value));
+    }
+    " 
