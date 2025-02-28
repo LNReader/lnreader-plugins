9829f9e4556af9d148df66cfd05ccfdd26183e07
Batorian <lukas.richter.berlin@web.de>
2025-02-28 17:59:06 +0100

    genesis: fix

--- src/plugins/english/genesis.ts
+++ src/plugins/english/genesis.ts
@@ -18,7 +18,7 @@ class Genesis implements Plugin.PluginBase {
   icon = 'src/en/genesis/icon.png';
   customCSS = 'src/en/genesis/customCSS.css';
   site = 'https://genesistudio.com';
-  version = '1.0.7';
+  version = '1.1.0';
 
   imageRequestInit?: Plugin.ImageRequestInit | undefined = {
     headers: {
@@ -39,7 +39,7 @@ class Genesis implements Plugin.PluginBase {
     { showLatestNovels, filters }: Plugin.PopularNovelsOptions,
   ): Promise<Plugin.SourceNovel[]> {
     if (pageNo !== 1) return [];
-    let link = `${this.site}/api/search?`;
+    let link = `${this.site}/api/novels/search?`;
     if (showLatestNovels) {
       link += 'sort=Recent';
     } else {
@@ -120,20 +120,21 @@ class Genesis implements Plugin.PluginBase {
 
       // Change string here if the chapters are stored under a different key
       const chapterKey = 'chapters';
+
       if (typeof value === 'object' && value !== null && chapterKey in value) {
-        const chapterData = this.decodeData(data[value[chapterKey]]);
+        const chapterData = data[value[chapterKey]];
+        const freeChapterData = chapterData.free;
+        const premiumChapterData = chapterData.premium;
 
-        // Object.values will give us an array of arrays (any[][])
-        const chapterArrays: any[][] = Object.values(chapterData);
+        const allChapterData = [
+          ...data[freeChapterData],
+          ...data[premiumChapterData],
+        ];
 
-        // Flatten and format the chapters
-        return chapterArrays.flatMap((chapters: any[]) => {
-          return chapters
-            .map((chapter: any) => this.formatChapter(chapter))
-            .filter(
-              (chapter): chapter is Plugin.ChapterItem => chapter !== null,
-            );
-        });
+        return allChapterData
+          .map((index: number) => data[index])
+          .map((chapter: any) => this.formatChapter(chapter, data))
+          .filter((chapter): chapter is Plugin.ChapterItem => chapter !== null);
       }
     }
 
@@ -141,27 +142,24 @@ class Genesis implements Plugin.PluginBase {
   }
 
   // Helper function to format an individual chapter
-  formatChapter(chapter: any): Plugin.ChapterItem | null {
-    const { id, chapter_title, chapter_number, required_tier, date_created } =
-      chapter;
-
+  formatChapter(chapter: any, data: any): Plugin.ChapterItem | null {
     // Ensure required fields are present and valid
     if (
-      id &&
-      chapter_title &&
-      chapter_number >= 0 &&
-      required_tier !== null &&
-      date_created
+      data[chapter.id] &&
+      data[chapter.chapter_title] &&
+      data[chapter.chapter_number] >= 0 &&
+      data[chapter.required_tier] !== null &&
+      data[chapter.date_created]
     ) {
-      const number = parseInt(chapter_number, 10) || 0;
-      const requiredTier = parseInt(required_tier, 10) || 0;
+      const number = parseInt(data[chapter.chapter_number], 10) || 0;
+      const requiredTier = parseInt(data[chapter.required_tier], 10) || 0;
 
       // Only process chapters with a 'requiredTier' of 0
       if (requiredTier === 0) {
         return {
-          name: `Chapter ${number}: ${chapter_title}`,
-          path: `/viewer/${id}`,
-          releaseTime: date_created,
+          name: `Chapter ${number} - ${data[chapter.chapter_title]}`,
+          path: `/viewer/${data[chapter.id]}`,
+          releaseTime: data[chapter.date_created],
           chapterNumber: number,
         };
       }
@@ -170,162 +168,219 @@ class Genesis implements Plugin.PluginBase {
     return null;
   }
 
-  decodeData(code: any) {
-    const offset = this.getOffsetIndex(code);
-    const params = this.getDecodeParams(code);
-    const constant = this.getConstant(code);
-    const data = this.getStringsArrayRaw(code);
-
-    const getDataAt = (x: number) => data[x - offset];
-
-    //reshuffle data array
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      try {
-        const some_number = this.applyDecodeParams(params, getDataAt);
-        if (some_number === constant) break;
-        else data.push(data.shift());
-      } catch (err) {
-        data.push(data.shift());
-      }
-    }
-
-    return this.getChapterData(code, getDataAt);
-  }
-
-  getOffsetIndex(code: string) {
-    // @ts-ignore
-    const string = /{(\w+)=\1-0x(?<offset>[0-9a-f]+);/.exec(code).groups.offset;
-    return parseInt(string, 16);
-  }
-
-  /**
-   * @returns {string[]}
-   */
-  getStringsArrayRaw(code: string) {
-    // @ts-ignore
-    let json = /function \w+\(\){var \w+=(?<array>\['.+']);/.exec(code).groups
-      .array;
-
-    //replace string single quotes with double quotes and add escaped chars
-    json = json.replace(/'(.+?)'([,\]])/g, (match, p1, p2) => {
-      return `"${p1.replace(/\\x([0-9a-z]{2})/g, (match: any, p1: string) => {
-        //hexadecimal unicode escape chars
-        return String.fromCharCode(parseInt(p1, 16));
-      })}"${p2}`;
-    });
-
-    return JSON.parse(json);
-  }
-
-  /**
-   * @returns {{offset: number, divider: number, negated: boolean}[][]}
-   */
-  getDecodeParams(code: string) {
-    // @ts-ignore
-    const jsDecodeInt = /while\(!!\[]\){try{var \w+=(?<code>.+?);/.exec(code)
-      .groups.code;
-    const decodeSections = jsDecodeInt.split('+');
-    const params = [];
-    for (const section of decodeSections) {
-      params.push(this.decodeParamSection(section));
-    }
-    return params;
-  }
-
-  /**
-   * @param {string} section
-   * @returns {{offset: number, divider: number, negated: boolean}[]}
-   */
-  decodeParamSection(section: string) {
-    const sections = section.split('*');
-    const params = [];
-    for (const section of sections) {
-      // @ts-ignore
-      const offsetStr = /parseInt\(\w+\(0x(?<offset>[0-9a-f]+)\)\)/.exec(
-        section,
-      ).groups.offset;
-      const offset = parseInt(offsetStr, 16);
-      // @ts-ignore
-      const dividerStr = /\/0x(?<divider>[0-9a-f]+)/.exec(section).groups
-        .divider;
-      const divider = parseInt(dividerStr, 16);
-      const negated = section.includes('-');
-      params.push({ offset, divider, negated });
-    }
-    return params;
-  }
-
-  getConstant(code: string) {
-    // @ts-ignore
-    const constantStr = /}}}\(\w+,0x(?<constant>[0-9a-f]+)\),/.exec(code).groups
-      .constant;
-    return parseInt(constantStr, 16);
-  }
-
-  getChapterData(
-    code: string,
-    getDataAt: { (x: number): any; (arg0: number): any },
-  ) {
-    let chapterDataStr =
-      // @ts-ignore
-      /\),\(function\(\){var \w+=\w+;return(?<data>{.+?});/.exec(code).groups
-        .data;
-
-    //replace hex with decimal
-    chapterDataStr = chapterDataStr.replace(/:0x([0-9a-f]+)/g, (match, p1) => {
-      const hex = parseInt(p1, 16);
-      return `: ${hex}`;
-    });
-
-    //replace ![] with false and !![] with true
-    chapterDataStr = chapterDataStr
-      .replace(/:!!\[]/g, ':true')
-      .replace(/:!\[]/g, ':false');
-
-    //replace string single quotes with double quotes and add escaped chars
-    chapterDataStr = chapterDataStr.replace(
-      /'(.+?)'([,\]}:])/g,
-      (match, p1, p2) => {
-        return `"${p1.replace(/\\x([0-9a-z]{2})/g, (match: any, p1: string) => {
-          //hexadecimal unicode escape chars
-          return String.fromCharCode(parseInt(p1, 16));
-        })}"${p2}`;
-      },
-    );
-
-    //parse the data getting methods
-    chapterDataStr = chapterDataStr.replace(
-      // @ts-ignore
-      /:\w+\(0x(?<offset>[0-9a-f]+)\)/g,
-      (match, p1) => {
-        const offset = parseInt(p1, 16);
-        return `:${JSON.stringify(getDataAt(offset))}`;
-      },
-    );
-
-    return JSON.parse(chapterDataStr);
-  }
-
-  /**
-   * @param {{offset: number, divider: number, negated: boolean}[][]} params
-   * @param {function(number): string} getDataAt
-   */
-  applyDecodeParams(
-    params: { offset: number; divider: number; negated: boolean }[][],
-    getDataAt: { (x: number): any; (arg0: any): string },
-  ) {
-    let res = 0;
-    for (const paramAdd of params) {
-      let resInner = 1;
-      for (const paramMul of paramAdd) {
-        resInner *= parseInt(getDataAt(paramMul.offset)) / paramMul.divider;
-        if (paramMul.negated) resInner *= -1;
-      }
-      res += resInner;
-    }
-    return res;
-  }
+  // // Helper function to extract and format chapters
+  // extractChapters(data: any): Plugin.ChapterItem[] {
+  //   for (const key in data) {
+  //     const value = data[key];
+  //
+  //     // Change string here if the chapters are stored under a different key
+  //     const chapterKey = 'chapters';
+  //     if (typeof value === 'object' && value !== null && chapterKey in value) {
+  //       const chapterData = this.decodeData(data[value[chapterKey]]);
+  //
+  //       // Object.values will give us an array of arrays (any[][])
+  //       const chapterArrays: any[][] = Object.values(chapterData);
+  //
+  //       // Flatten and format the chapters
+  //       return chapterArrays.flatMap((chapters: any[]) => {
+  //         return chapters
+  //           .map((chapter: any) => this.formatChapter(chapter))
+  //           .filter(
+  //             (chapter): chapter is Plugin.ChapterItem => chapter !== null,
+  //           );
+  //       });
+  //     }
+  //   }
+  //
+  //   return [];
+  // }
+  //
+  // // Helper function to format an individual chapter
+  // formatChapter(chapter: any): Plugin.ChapterItem | null {
+  //   const { id, chapter_title, chapter_number, required_tier, date_created } =
+  //     chapter;
+  //
+  //   // Ensure required fields are present and valid
+  //   if (
+  //     id &&
+  //     chapter_title &&
+  //     chapter_number >= 0 &&
+  //     required_tier !== null &&
+  //     date_created
+  //   ) {
+  //     const number = parseInt(chapter_number, 10) || 0;
+  //     const requiredTier = parseInt(required_tier, 10) || 0;
+  //
+  //     // Only process chapters with a 'requiredTier' of 0
+  //     if (requiredTier === 0) {
+  //       return {
+  //         name: `Chapter ${number}: ${chapter_title}`,
+  //         path: `/viewer/${id}`,
+  //         releaseTime: date_created,
+  //         chapterNumber: number,
+  //       };
+  //     }
+  //   }
+  //
+  //   return null;
+  // }
+  //
+  // decodeData(code: any) {
+  //   const offset = this.getOffsetIndex(code);
+  //   const params = this.getDecodeParams(code);
+  //   const constant = this.getConstant(code);
+  //   const data = this.getStringsArrayRaw(code);
+  //
+  //   const getDataAt = (x: number) => data[x - offset];
+  //
+  //   //reshuffle data array
+  //   // eslint-disable-next-line no-constant-condition
+  //   while (true) {
+  //     try {
+  //       const some_number = this.applyDecodeParams(params, getDataAt);
+  //       if (some_number === constant) break;
+  //       else data.push(data.shift());
+  //     } catch (err) {
+  //       data.push(data.shift());
+  //     }
+  //   }
+  //
+  //   return this.getChapterData(code, getDataAt);
+  // }
+  //
+  // getOffsetIndex(code: string) {
+  //   // @ts-ignore
+  //   const string = /{(\w+)=\1-0x(?<offset>[0-9a-f]+);/.exec(code).groups.offset;
+  //   return parseInt(string, 16);
+  // }
+  //
+  // /**
+  //  * @returns {string[]}
+  //  */
+  // getStringsArrayRaw(code: string) {
+  //   // @ts-ignore
+  //   let json = /function \w+\(\){var \w+=(?<array>\['.+']);/.exec(code).groups
+  //     .array;
+  //
+  //   //replace string single quotes with double quotes and add escaped chars
+  //   json = json.replace(/'(.+?)'([,\]])/g, (match, p1, p2) => {
+  //     return `"${p1.replace(/\\x([0-9a-z]{2})/g, (match: any, p1: string) => {
+  //       //hexadecimal unicode escape chars
+  //       return String.fromCharCode(parseInt(p1, 16));
+  //     })}"${p2}`;
+  //   });
+  //
+  //   return JSON.parse(json);
+  // }
+  //
+  // /**
+  //  * @returns {{offset: number, divider: number, negated: boolean}[][]}
+  //  */
+  // getDecodeParams(code: string) {
+  //   // @ts-ignore
+  //   const jsDecodeInt = /while\(!!\[]\){try{var \w+=(?<code>.+?);/.exec(code)
+  //     .groups.code;
+  //   const decodeSections = jsDecodeInt.split('+');
+  //   const params = [];
+  //   for (const section of decodeSections) {
+  //     params.push(this.decodeParamSection(section));
+  //   }
+  //   return params;
+  // }
+  //
+  // /**
+  //  * @param {string} section
+  //  * @returns {{offset: number, divider: number, negated: boolean}[]}
+  //  */
+  // decodeParamSection(section: string) {
+  //   const sections = section.split('*');
+  //   const params = [];
+  //   for (const section of sections) {
+  //     // @ts-ignore
+  //     const offsetStr = /parseInt\(\w+\(0x(?<offset>[0-9a-f]+)\)\)/.exec(
+  //       section,
+  //     ).groups.offset;
+  //     const offset = parseInt(offsetStr, 16);
+  //     // @ts-ignore
+  //     const dividerStr = /\/0x(?<divider>[0-9a-f]+)/.exec(section).groups
+  //       .divider;
+  //     const divider = parseInt(dividerStr, 16);
+  //     const negated = section.includes('-');
+  //     params.push({ offset, divider, negated });
+  //   }
+  //   return params;
+  // }
+  //
+  // getConstant(code: string) {
+  //   // @ts-ignore
+  //   const constantStr = /}}}\(\w+,0x(?<constant>[0-9a-f]+)\),/.exec(code).groups
+  //     .constant;
+  //   return parseInt(constantStr, 16);
+  // }
+  //
+  // getChapterData(
+  //   code: string,
+  //   getDataAt: { (x: number): any; (arg0: number): any },
+  // ) {
+  //   let chapterDataStr =
+  //     // @ts-ignore
+  //     /\),\(function\(\){var \w+=\w+;return(?<data>{.+?});/.exec(code).groups
+  //       .data;
+  //
+  //   //replace hex with decimal
+  //   chapterDataStr = chapterDataStr.replace(/:0x([0-9a-f]+)/g, (match, p1) => {
+  //     const hex = parseInt(p1, 16);
+  //     return `: ${hex}`;
+  //   });
+  //
+  //   //replace ![] with false and !![] with true
+  //   chapterDataStr = chapterDataStr
+  //     .replace(/:!!\[]/g, ':true')
+  //     .replace(/:!\[]/g, ':false');
+  //
+  //   //replace string single quotes with double quotes and add escaped chars
+  //   chapterDataStr = chapterDataStr.replace(
+  //     /'(.+?)'([,\]}:])/g,
+  //     (match, p1, p2) => {
+  //       return `"${p1.replace(/\\x([0-9a-z]{2})/g, (match: any, p1: string) => {
+  //         //hexadecimal unicode escape chars
+  //         return String.fromCharCode(parseInt(p1, 16));
+  //       })}"${p2}`;
+  //     },
+  //   );
+  //
+  //   //parse the data getting methods
+  //   chapterDataStr = chapterDataStr.replace(
+  //     // @ts-ignore
+  //     /:\w+\(0x(?<offset>[0-9a-f]+)\)/g,
+  //     (match, p1) => {
+  //       const offset = parseInt(p1, 16);
+  //       return `:${JSON.stringify(getDataAt(offset))}`;
+  //     },
+  //   );
+  //
+  //   return JSON.parse(chapterDataStr);
+  // }
+  //
+  // /**
+  //  * @param {{offset: number, divider: number, negated: boolean}[][]} params
+  //  * @param {function(number): string} getDataAt
+  //  */
+  // applyDecodeParams(
+  //   params: { offset: number; divider: number; negated: boolean }[][],
+  //   getDataAt: { (x: number): any; (arg0: any): string },
+  // ) {
+  //   let res = 0;
+  //   for (const paramAdd of params) {
+  //     let resInner = 1;
+  //     for (const paramMul of paramAdd) {
+  //       resInner *= parseInt(getDataAt(paramMul.offset)) / paramMul.divider;
+  //       if (paramMul.negated) resInner *= -1;
+  //     }
+  //     res += resInner;
+  //   }
+  //   return res;
+  // }
 
   async parseChapter(chapterPath: string): Promise<string> {
     const url = `${this.site}${chapterPath}/__data.json?x-sveltekit-invalidated=001`;
@@ -347,7 +402,7 @@ class Genesis implements Plugin.PluginBase {
     pageNo: number,
   ): Promise<Plugin.SourceNovel[]> {
     if (pageNo !== 1) return [];
-    const url = `${this.site}/api/search?serialization=All&sort=Popular&title=${searchTerm}`;
+    const url = `${this.site}/api/novels/search?serialization=All&sort=Popular&title=${searchTerm}`;
     const json = await fetchApi(url).then(r => r.json());
     return this.parseNovels(json);
   }
