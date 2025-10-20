09af4e492dfb99adc8f610a1c4c85e8f48135939
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2025-10-20 23:53:42 +0530

    fix: Update Plugin Link for Backward Compatibility

--- scripts/build-plugin-manifest.js
+++ scripts/build-plugin-manifest.js
@@ -19,7 +19,8 @@ const USER_CONTENT_LINK = process.env.USER_CONTENT_BASE
   : `https://raw.githubusercontent.com/${USERNAME}/${REPO}/${CURRENT_BRANCH}`;
 
 const STATIC_LINK = `${USER_CONTENT_LINK}/public/static`;
-const PLUGIN_LINK = `${USER_CONTENT_LINK}/.js/plugins`;
+// Use legacy .js/src/plugins path for backward compatibility
+const PLUGIN_LINK = `${USER_CONTENT_LINK}/.js/src/plugins`;
 
 const DIST_DIR = '.dist';
 
